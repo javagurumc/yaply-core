cb68ae07edc166aead59a6e1f60211b9395916bd
Martins Ceske <martins.ceske@gmail.com>
2025-11-16 13:36:23 +0200

    initial setup

---
+++ src/main/java/eu/companon/clarity_walk_core/ClarityWalkCoreApplication.java
@@ -0,0 +1,13 @@
+package eu.companon.clarity_walk_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClarityWalkCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClarityWalkCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/companon/clarity_walk_core/ClarityWalkCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.companon.clarity_walk_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClarityWalkCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
